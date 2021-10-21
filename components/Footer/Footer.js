d7b2a7e204da0531eb88605f797f7d5022028466
Desyscott <Mrfallback055@gmail.com>
2021-10-21 22:47:46 +0000

    initial setup

---
+++ components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+function Footer() {
+  return (
+    <View>
+      <Text>footer</Text>
+    </View>
+  );
+}
+
+export default Footer;
---
+++ components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+function Header() {
+  return (
+    <View>
+      <Text>header</Text>
+    </View>
+  );
+}
+
+export default Header;
